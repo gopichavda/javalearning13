bdf39f16ddbd051dae5bfdcaf835be4b560bec0b
Aarti chavda <aarti22rajput@gmail.com>
2022-07-20 22:38:40 +0100

    javalearning13

--- java13.java
+++ java13.java
@@ -2,6 +2,10 @@ package jv13;
 
 public class java13 {
     public static void main(String[] args) {
-        System.out.println("data type byte");
+        System.out.println("data type");
+        System.out.println("data file");
+        System.out.println("data ");
+        System.out.println("aarti");
+
     }
 }
